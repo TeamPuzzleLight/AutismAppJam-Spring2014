7756bc9081249ab37254c5d02c13f4ca3ba5bf31
Leo Jiapeil Lin <jiapeil@uci.edu>
2014-04-25 13:46:52 -0700

    let this be the last commit before judging :D

--- js/game.js
+++ js/game.js
@@ -1,7 +1,7 @@
 $(function() {
     $('#startgame').click(function() {
 
-        $('body').append('<div id="instruction"><h2 id="content" style="margin-top:50px; border: 3px, solid, black; background:white;">Please add the items into your basket by clicking on them</h2></div>');
+        $('body').append('<div id="instruction"><h2 id="content" style="margin-top:10px; border: 3px, solid, black;background:white;">Please add the items into your basket by clicking on them</h2></div>');
         //apple, banana, pear, carrot, tomato
         var foodimages = ["images/apple.jpg", "images/banana.jpg", "images/pear.png", "images/tomato.jpg", "images/carrot.jpg"];
         var foodCount = [2, 3, 1, 2, 1];
@@ -23,7 +23,7 @@ $(function() {
             var s = "";
             for (var i = 0; i < 5; i++) {
                 if (foodCount[i] < 0) {
-                    s += "You took too many <strong><u>" + foodName[i] + "</u></strong>. Click on one of the <strong><u>" + foodName[i] + "</u></strong> in the Basket to remove it. <br><br>";
+                    s += "You took too many <strong><u>" + foodName[i] + "</u></strong>. Click on one of the <strong><u>" + foodName[i] + "</u></strong> in the Basket to remove it.";
                 }
             }
             document.getElementById("content").innerHTML = s;
@@ -37,7 +37,6 @@ $(function() {
                 var it = null;
                 var index = $(this).index() - 1;
                 foodCount[index] -= 1;
-                //                $($('#shoppinglist li span')[index]).html(food[index] < 0 ? '0' : food[index]);
 
                 var newimage = '<img src=' + foodimages[index] + " class=foodinbasket alt=" + "'" + index + "'" + "/>";
                 console.log(newimage);

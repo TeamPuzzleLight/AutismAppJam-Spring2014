db7dc0a38f99e735682f801dd19b5b604da20618
Leo Jiapeil Lin <jiapeil@uci.edu>
2014-04-25 10:09:57 -0700

    last night

--- js/game.js
+++ js/game.js
@@ -2,7 +2,7 @@ $(function() {
     $('#startgame').click(function() {
 
         $('body').append('<div id="instruction"><h2 id="content" style="margin-top:50px; border: 3px, solid, black; background:white;">Please add the items into your basket by clicking on them</h2></div>');
-        //apple, banana, broccoli, carrot, tomato
+        //apple, banana, pear, carrot, tomato
         var foodimages = ["images/apple.jpg", "images/banana.jpg", "images/pear.png", "images/tomato.jpg", "images/carrot.jpg"];
         var foodCount = [2, 3, 1, 2, 1];
         var count = 0;
@@ -23,7 +23,7 @@ $(function() {
             var s = "";
             for (var i = 0; i < 5; i++) {
                 if (foodCount[i] < 0) {
-                    s += "You took too many " + foodName[i] + "<br><br>";
+                    s += "You took too many <strong><u>" + foodName[i] + "</u></strong>. Click on one of the <strong><u>" + foodName[i] + "</u></strong> in the Basket to remove it. <br><br>";
                 }
             }
             document.getElementById("content").innerHTML = s;

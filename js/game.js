e954e3fa4062f6a3457da5d16342f78c308ca343
kctseng <kctseng2012@gmail.com>
2014-04-24 23:20:43 -0700

    Fix the items on shelf :D

--- js/game.js
+++ js/game.js
@@ -1,12 +1,12 @@
 $(function() {
     $('#startgame').click(function() {
 
-        $('body').append('<div id="instruction"><h2 id="content" style="border: 3px, solid, black; background:white;">Please add the items into your basket by clicking on them</h2></div>');
+        $('body').append('<div id="instruction"><h2 id="content" style="margin-top:50px; border: 3px, solid, black; background:white;">Please add the items into your basket by clicking on them</h2></div>');
         //apple, banana, broccoli, carrot, tomato
-        var foodimages = ["images/apple.jpg", "images/banana.jpg", "images/Broccoli.jpg", "images/carrot.jpg", "images/tomato.jpg"];
+        var foodimages = ["images/apple.jpg", "images/banana.jpg", "images/pear.png", "images/tomato.jpg", "images/carrot.jpg"];
         var foodCount = [2, 3, 1, 2, 1];
         var count = 0;
-        var foodName = ["Apple", "Banana", "Broccoli", "Carrot", "Tomato"];
+        var foodName = ["apples", "bananas", "pears", "tomatos", "carrots"];
         var allZero = function() {
             for (var i = 0; i < 5; i++) {
                 if (foodCount[i] != 0) {
@@ -23,7 +23,7 @@ $(function() {
             var s = "";
             for (var i = 0; i < 5; i++) {
                 if (foodCount[i] < 0) {
-                    s += "You Took Too many" + foodName[i] + "<br>";
+                    s += "You took too many " + foodName[i] + "<br><br>";
                 }
             }
             document.getElementById("content").innerHTML = s;
